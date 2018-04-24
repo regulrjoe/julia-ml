76ff6b358725b308d5e500a31931f7a3caa02e6e
Jorge Vazquez Mellado <joevzq@gmail.com>
2018-04-24 13:03:49 -0500

    Write cost function. update Hypothesis function. Add necessary includes. Set as module with exports.

--- logistic_regression.jl
+++ logistic_regression.jl
@@ -1,21 +1,41 @@
+module LogisticRegression
 
+include("feature_scaling.jl")
+include("gradient_descent.jl")
+include("helpers.jl")
 
+export train, run
 
+function train(X::Array{Float64,2}, Y::Array{Float64,1})
+end
 
-
-
-
+# Cost function
+# 1m * ()−y'log⁡(h) − (1−y)' * log⁡(1−h))
+# Input:
+#   X -> Matrix of input data set
+#   Y -> Vector of output of training set
+#   T -> Vector of parameters to evaluate
+#   h -> Hypothesis function to evaluate
+function J(X::Array{Float64,2}, Y::Array{Float64,1}, T::Array{Float64,1})
+    m = size(X,1)
+    1/m * (-Y' * log.(h(X,T)) - (1 - Y)' * log.(1 - h(X,T)))
+end
 
 # Logistic regression hypothesis function
 # 1 / (1 + e^-(0'x))
 # Input:
 #   X -> nxm Matrix of input data set
 #   T -> n Vector of parameters to evaluate
-function h(X, T)
-    X = Helpers.check_ones_col(X, T)
+
+# Applied to vector
+function h(x::Array{Float64,1}, T::Array{Float64,1})
+    g(T' * x)
+end
+# Applied to matrix
+function h(X::Array{Float64,2}, T::Array{Float64,1})
     Y = Array{Float64}(size(X,1))
     for i = 1:size(X,1)
-        Y[i] = Sigmoid.g(T' * X[i,:])
+        Y[i] = g(T' * X[i,:])
     end
     return Y
 end
@@ -23,4 +43,6 @@ end
 # Sigmoid function for logistic regression
 function g(z)
     1 / (1 + e^-z)
+end
+
 end
