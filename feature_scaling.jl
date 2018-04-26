eb535d4ea579428bbfe20d7d3bfa1e883727ca98
Jorge Vazquez Mellado <joevzq@gmail.com>
2018-04-26 00:20:09 -0500

    Create and implement Parameters type

--- feature_scaling.jl
+++ feature_scaling.jl
@@ -1,26 +1,30 @@
 module FeatureScaling
 
-export fnormalize!
+export fnormalize!, NormalizationParams
+
+# Normalization Parameters
+mutable struct Params{A<:Array{Float64,1}}
+    mu::A # means for normalization
+    sd::A # standard deviations for normalization
+end
 
 # Feature scaling for matrix with mean normalization
 # Input:
 #   X -> Matrix of input data set
 # Output:
-#   mu -> Vector of mean of each feature
-#   sd -> Vector of standard deviation of each feature
+#   params -> Normalization Parameters
 function fnormalize!(X::Array{Float64, 2})
     m = size(X, 1) # Number of input entries
     n = size(X, 2) # Number of features
-    mu = zeros(n) # Vector of feature's average
-    sd = zeros(n) # Vector of feature's standard deviation
+    params = Params(zeros(n), zeros(n))
     for i = 1:n
-        mu[i] = mean(X[:,i])
-        sd[i] = std(X[:,i])
-        if sd[i] != 0.0
-            X[:,i] = (X[:,i] - mu[i]) / sd[i]
+        params.mu[i] = mean(X[:,i])
+        params.sd[i] = std(X[:,i])
+        if params.sd[i] != 0.0
+            X[:,i] = (X[:,i] - params.mu[i]) / params.sd[i]
         end
     end
-    return mu, sd
+    return params
 end
 
 # Feature scaling with normalization parameters known
@@ -30,18 +34,18 @@ end
 #   sd -> Vector of standard deviation for each feature
 
 # Vector normalization
-function fnormalize!(x::Array{Float64,1}, mu::Array{Float64,1}, sd::Array{Float64,1})
+function fnormalize!(x::Array{Float64,1}, params::Params)
     for i = 1:length(x)
-        if sd[i] != 0.0
-            x[i] = (x[i] - mu[i]) / sd[i]
+        if params.sd[i] != 0.0
+            x[i] = (x[i] - params.mu[i]) / params.sd[i]
         end
     end
 end
 # Matrix normalization
-function fnormalize!(X::Array{Float64,2}, mu::Array{Float64,1}, sd::Array{Float64,1})
+function fnormalize!(X::Array{Float64,2}, params::Params)
     for i = 1:size(X,2)
-        if sd[i] != 0.0
-            X[:,i] = (X[:,i] - mu[i]) / sd[i]
+        if params.sd[i] != 0.0
+            X[:,i] = (X[:,i] - params.mu[i]) / params.sd[i]
         end
     end
 end
