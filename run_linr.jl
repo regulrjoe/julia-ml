d3238581c1a268c92d86e58225f29bc45033b368
Jorge Vazquez Mellado <joevzq@gmail.com>
2018-04-26 19:16:28 -0500

    Delete RunML.jl and create run_linr.jl to run Linear Regression algorithm

--- run_linr.jl
+++ run_linr.jl
@@ -7,8 +7,8 @@ using CSV, DataFrames, StatPlots
 
 # Data reading
 data = CSV.read("machine-learning-ex1/ex1/ex1data2.txt", datarow = 1)
-X = convert(Array{Float64}, data[:, 1:size(data,2)-1]) # First to second-to-last columns
-Y = convert(Array{Float64}, data[:, size(data,2)]) # Last column
+X = convert(Array{Float64}, data[:, 1:end-1) # First to second-to-last columns
+Y = convert(Array{Float64}, data[:, end]) # Last column
 
 # Data manipulation
 X = Helpers.check_matrix_float64(X)
@@ -20,7 +20,7 @@ lr = LinearRegression
 @time output = lr.train(X, Y)
 
 if size(output, 1) == 1
-    Y2 = lr.run(X, output[1])
+    Y2 = lr.predict(X, output[1])
 elseif size(output, 1) == 2
-    Y2 = lr.run(X, output[1], output[2])
+    Y2 = lr.predict(X, output[1], output[2])
 end
