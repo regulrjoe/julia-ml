fd6009e0b75b644a308b91c25a4d3bc7578a0f3b
Jorge Vazquez Mellado <joevzq@gmail.com>
2018-04-27 17:08:03 -0500

    Structure run_logr.jl

--- src/run_logr.jl
+++ src/run_logr.jl
@@ -1,10 +1,10 @@
-push!(LOAD_PATH, pwd())
-
 include("logistic_regression.jl")
 include("helpers.jl")
+include("feature_scaling.jl")
+include("plot_boundary.jl")
 
 using CSV, DataFrames, StatPlots
-gr(size = (400, 400))
+plotly()
 
 # Data reading
 data = CSV.read("machine-learning-ex2/ex2/ex2data1.txt", datarow = 1)
@@ -13,21 +13,28 @@ Y = convert(Array{Float64}, data[:, end]) # Last column
 Positives = data[data[:, :Column3] .== 1, :]
 Negatives = data[data[:, :Column3] .== 0, :]
 
-# Plot data
-@df Positives scatter(:Column1, :Column2, colour = :blue)
-@df Negatives scatter!(:Column1, :Column2, colour = :red)
-
 # Data manipulation
 X = Helpers.check_matrix_float64(X)
 X = Helpers.check_ones_col(X)
 
-lr = LinearRegression
+lr = LogisticRegression
 
 # Linear regression
-@time output = lr.train(X, Y)
+@time output = lr.train(X, Y, alpha = 0.3, max_its = 2000, epsilon = 0.000001)
 
-if size(output, 1) == 1
-    Y2 = lr.run(X, output[1])
-elseif size(output, 1) == 2
-    Y2 = lr.run(X, output[1], output[2])
-end
+println("Thetas: ", output[1])
+Y2 = lr.predict(
+    [1.0; 45.0; 85.0],
+    output[1],
+    output[2]
+)
+
+# Plot data
+XP = Helpers.check_ones_col(convert(Array{Float64}, Positives[:,1:end-1]))
+XN = Helpers.check_ones_col(convert(Array{Float64}, Negatives[:,1:end-1]))
+FeatureScaling.fnormalize!(X, FeatureScaling.Params(output[2].mu, output[2].sd))
+FeatureScaling.fnormalize!(XP, FeatureScaling.Params(output[2].mu, output[2].sd))
+FeatureScaling.fnormalize!(XN, FeatureScaling.Params(output[2].mu, output[2].sd))
+scatter(XP[:,2], XP[:,3], colour = :blue)
+scatter!(XN[:,2], XN[:,3], colour = :red)
+PlotBoundary.plot_b!(X, output[1])
